6c91f08535f1c29f2a0f48a1e2a63270ae23eda9
kanuj3766 <120703612+kanuj3766@users.noreply.github.com>
2024-02-16 17:58:54 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,14 +1,14 @@
 let score = "Anuj Kumar"
 // console.log(typeof score);
 // console.log(typeof score);
-
+/*
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
 
-/*
+
 let valueInNumber = Number(score)
  //console.log(typeof valueInNumber);
  //console.log(valueInNumber);
@@ -30,17 +30,19 @@ let valueInNumber = Number(score)
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+ console.log(negValue);
 
-// console.log(2 + 2);
-// console.log(2 - 2);
-// console.log(2 * 2);
-// console.log(2 / 2);
+ //console.log(2 + 2);
+ //console.log(2 - 2);
+ //console.log(2 * 2);
+ //console.log(2 / 2);
+ //console.log(2 % 3);
+ 
 
 let str1 = "hello"
-let str2 = "Anuj"
+let str2 = " Anuj"
 let str3 = str1 + str2
-// console.log(str3);
+ console.log(str3);
 
 //console.log("1" + 2);
 //console.log(1 + "2");
@@ -48,10 +50,13 @@ let str3 = str1 + str2
 //console.log(1 + 2 + "2");
 //console.log((3 + 4) * 5 %3);
 
-//console.log(+true);
-//console.log((+ "");
+console.log(+true);
+console.log((+ ""));
+
+
 
 let gameCounter = 100
 ++gameCounter;
 console.log(gameCounter);
+
 */
