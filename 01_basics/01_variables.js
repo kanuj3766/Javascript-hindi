db99b0e3f8722ab47d7f67d4e5fba55f0e92ab7c
kanuj3766 <120703612+kanuj3766@users.noreply.github.com>
2024-02-16 14:01:07 +0000

    container change done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,6 @@
 const accountId = 748000
-let accountEmail = "kanuj3766@gmail.com"
-var accountPassword = "54321"
+let accountEmail = "kanuj3766@gmail.com" //  Variables declared by let are only available inside the block where they're defined.
+var accountPassword = "54321" // Variables declared by var are available throughout the function in which they're declared.
 accountCity =  "Raipur"
 let accountState;
 
@@ -13,5 +13,9 @@ console.log(accountId);
 Prefer not to use var
 because of issue in block scope and functional scope 
 */
+/*
+comment- it can be used to add single as well as multi-line comment so it is more convenient.
+*/
+
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
