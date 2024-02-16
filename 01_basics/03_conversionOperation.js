4a7480a345d00b402394e5972113cd6dd5ef48ac
kanuj3766 <120703612+kanuj3766@users.noreply.github.com>
2024-02-16 17:37:57 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,57 @@
+let score = "Anuj Kumar"
+// console.log(typeof score);
+// console.log(typeof score);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+/*
+let valueInNumber = Number(score)
+ //console.log(typeof valueInNumber);
+ //console.log(valueInNumber);
+// "33" => 33
+// "33abc" => NaN
+// true => 1;
+// false => 0;
+
+
+ let isLoggedIn = "Anuj kumar" // 1 => true; 0 => false; "" => false; "Anuj kumar" => true
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+ 
+
+
+
+
+//******************************** Operation *************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+
+let str1 = "hello"
+let str2 = "Anuj"
+let str3 = str1 + str2
+// console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+//console.log((3 + 4) * 5 %3);
+
+//console.log(+true);
+//console.log((+ "");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+*/
