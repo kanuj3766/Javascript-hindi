9d5c46e8de5fecfdc1a351dc7325de094446d07c
kanuj3766 <120703612+kanuj3766@users.noreply.github.com>
2024-02-17 14:04:46 +0000

    camprision done

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -19,3 +19,5 @@ console.log(null >= 0); // output => true
 console.log(undefined == 0); // output => false
 console.log(undefined > 0);  // output => false
 console.log(undefined < 0);  // output => false
+
+console.log("2" === 2);      // output => false
